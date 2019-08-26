ccd6052c71c2c38805124fe95e85b158bc91504b
Yi Wang <yi.wang@huskers.unl.edu>
2019-08-26 17:21:52 -0500

    modify select_region.py

--- select_region.py
+++ select_region.py
@@ -1,5 +1,8 @@
+"""
+Created on August 26, 2019
 
-
+@author: Yi Wang
+"""
 
 import numpy as np
 from netCDF4 import Dataset
@@ -40,3 +43,63 @@ def is_in_the_region(lat, lon, region):
 
     return flag
 
+def MODIS_in_the_region(filename, region):
+    """ Check if there is at least one pixel of the granule (filename)
+    that is in the region.
+
+    Parameters
+    ----------
+    filename : str
+        MODIS filename
+    region : tuple-like
+        Define the region.
+        (min_lat, min_lon, max_lat, max)
+
+    Returns
+    -------
+    flag : logical
+
+    """
+
+    # Read latitude and longitude.
+    f = Dataset(filename, 'r')
+    lat = f.variables['Latitude'][:]
+    lon = f.variables['Longitude'][:]
+    f.close()
+
+    # Check every pixel.
+    all_pixel_flag = is_in_the_region(lat, lon, region)
+
+    # At least one pixel.
+    flag = np.any(all_pixel_flag)
+
+    return flag
+
+def all_files_in_the_region(all_files, region):
+    """ Return all the filenames that has at least one pixel of the 
+        granule (filename) that is in the region.
+
+    Parameters
+    ----------
+    all_files : tuple-like
+        Element is filename.
+    region : tuple-like
+        Define the region.
+        (min_lat, min_lon, max_lat, max)
+
+    Returns
+    -------
+    select_file : list
+        All files that are in the region
+
+    """
+
+    select_file = []
+    for filename in all_files:
+
+        flag = MODIS_in_the_region(filename, region)
+
+        if flag:
+            select_file.append(filename)
+
+    return select_file
