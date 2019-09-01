d246f6d432068c45b7dc36edb499fd133de239a7
Yi Wang <yi.wang@huskers.unl.edu>
2019-09-01 10:39:23 -0500

    add get_granule_corner get_granule_center get_satellite_metadata.py

---
+++ get_satellite_metadata.py
@@ -0,0 +1,55 @@
+"""
+Created on August 31, 2019
+
+@author: Yi Wang
+"""
+
+from netCDF4 import Dataset
+
+def get_mxd04(filename):
+    """Get Metadata from MxD04 file
+
+    Returns:
+    --------
+    """
+
+    f = Dataset(filename, 'r')
+
+    # read CoreMetadata.0
+    core_md = f.getncattr('CoreMetadata.0')
+    core_md = core_md.split('\n')
+
+    # read ArchiveMetada.0
+    arc_md = f.getncattr('ArchiveMetadata.0')
+    arc_md = arc_md.split('\n')
+
+    f.close()
+
+    # dictionary to save metadata
+    metadata = {}
+
+    # GRINGPOINTLONGITUDE
+    i = arc_md.index('        OBJECT                 = GRINGPOINTLONGITUDE')
+    tmp = arc_md[i+3]
+    tmp = tmp.split()
+    GRINGPOINTLONGITUDE = (
+        float(tmp[2][1:-1]),
+        float(tmp[3][:-1]),
+        float(tmp[4][:-1]),
+        float(tmp[5][:-1]),
+        )
+    metadata['GRINGPOINTLONGITUDE'] = GRINGPOINTLONGITUDE
+
+    # GRINGPOINTLATITUDE
+    i = arc_md.index('        OBJECT                 = GRINGPOINTLATITUDE')
+    tmp = arc_md[i+3]
+    tmp = tmp.split()
+    GRINGPOINTLATITUDE = (
+        float(tmp[2][1:-1]),
+        float(tmp[3][:-1]),
+        float(tmp[4][:-1]),
+        float(tmp[5][:-1]),
+        )
+    metadata['GRINGPOINTLATITUDE'] = GRINGPOINTLATITUDE
+
+    return metadata
--- pro_satellite.py
+++ pro_satellite.py
@@ -32,3 +32,77 @@ def calculate_pixel_edge2(lat_c, lon_c):
     lon_e = calculate_pixel_edge(lon_c)
 
     return lat_e, lon_e
+
+def get_granule_corner(lat, lon):
+    """Get the latitude and longitude of granule four corners.
+    """
+
+    if (lat.shape != lon.shape):
+        print('get_granule_corner: the dimensions of lat and lon are \
+inconsistent')
+
+    NY = lat.shape[0]
+    NX = lat.shape[1]
+
+    # indexes of the four corners
+    corn_index = ( (0, 0), (0, NX-1), (NY-1, NX-1), (NY-1, 0) )
+
+    # latitude and longitude of the four corns
+    corn = []
+    for cind in range(len(corn_index)):
+
+        i = corn_index[cind][0]
+        j = corn_index[cind][1]
+
+        corn_lat_lon = (lat[i,j], lon[i,j])
+
+        corn.append(corn_lat_lon)
+    
+    return tuple(corn)
+
+def get_granule_center(lat, lon):
+    """Get the latitude and longitude of granule four center
+    """
+
+    if (lat.shape != lon.shape):
+        print('get_granule_corner: the dimensions of lat and lon are \
+inconsistent')
+
+    NY = lat.shape[0]
+    NX = lat.shape[1]
+
+    # center at Y direction
+    if ( (NY % 2) == 0 ):
+
+        i2 = NY // 2
+        i1 = i2 - 1
+
+        lat_y = (lat[i1,:] + lat[i2,:]) * 0.5
+        lon_y = (lon[i1,:] + lon[i2,:]) * 0.5
+
+    else:
+
+        i = NY // 2
+
+        lat_y = lat[i,:]
+        lon_y = lon[i,:]
+
+    # center at X direction
+    if ( (NX % 2) == 0 ):
+
+        j2 = NX // 2
+        j1 = j2 - 1
+
+        lat_c = (lat_y[j1] + lat_y[j2]) * 0.5
+        lon_c = (lon_y[j1] + lon_y[j2]) * 0.5
+
+    else:
+
+        j = NX // 2
+
+        lat_c = lat_y[j]
+        lon_c = lon_y[j]
+
+    return (lat_c, lon_c)
+
+
