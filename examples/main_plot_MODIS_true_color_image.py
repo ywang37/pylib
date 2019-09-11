c4ec7fbb9912862e79f06d12b958fc6877b2accd
Yi Wang <ywang466@argon-login-1.hpc>
2019-09-11 13:41:26 -0500

    add main_plot_MODIS_true_color_image.py

---
+++ examples/main_plot_MODIS_true_color_image.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from mylib.cartopy_plot import add_geoaxes, pcolormesh
+from mylib.read_MODIS import get_modis_1km_rgb
+
+filename = './MYD021KM.A2015344.0725.006.2015344193947.hdf'
+
+# get data
+lat, lon, rgb = get_modis_1km_rgb(filename)
+
+# plot
+fig = plt.figure()
+xtick=np.arange(-180, 180.1, 5)
+ytick=np.arange(-90, 90.1, 5)
+ax = add_geoaxes(fig, 111, xtick=xtick, ytick=ytick)
+title = '.'.join(filename.split('/')[-1].split('.')[0:4])
+pcolormesh(ax, lon, lat, rgb, title=title)
+
+figname = './' + title + '.png'
+plt.savefig(figname, format='png', dpi=300)
