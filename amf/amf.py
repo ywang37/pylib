7da0358b314f910ea8eecf3fb64979e222f14e2e
Yi Wang <ywang466@argon-login-1.hpc>
2020-01-16 17:55:10 -0600

    modify amf.py and trop.py, but they are still not tested

--- amf/amf.py
+++ amf/amf.py
@@ -7,6 +7,9 @@ Created on Janunary 16, 2020
 import numpy as np
 from scipy import interpolate
 
+#
+#------------------------------------------------------------------------------
+#
 def AMF_one(layer_val, SW):
     """ Calculate air mass factor
 
@@ -39,7 +42,9 @@ def AMF_one(layer_val, SW):
     out_dict['AMF'] = AMF
 
     return out_dict
-
+#
+#------------------------------------------------------------------------------
+#
 def VCD_AK_one(layer_val, AK):
     """ Calculate a vertical column density that can be compared
     to the satellite retrieval.
@@ -63,7 +68,9 @@ def VCD_AK_one(layer_val, AK):
     V_AK = np.sum( layer_val * AK )
 
     return V_AK
-
+#
+#------------------------------------------------------------------------------
+#
 def SW_AK_intep_one(SW_AK, SW_AK_press, new_press):
     """ Interpolate scattering weight or averaging kernel
     to new pressure levels.
@@ -98,3 +105,165 @@ def SW_AK_intep_one(SW_AK, SW_AK_press, new_press):
     new_SW_AK = func(tmp_new_press)
 
     return new_SW_AK
+#
+#------------------------------------------------------------------------------
+#
+def AMF_trop_one(layer_val, PEdge_Bot, SW_AK, SW_AK_press, ind_l=None,
+        P_tropopause=None, var='AMF'):
+    """ Calculate tropospheric
+    (1) Air mass factor if *var* is 'AMF', or
+    (2) Vertical column density that can be compared to the 
+        satellite retrieval if *var* is 'VCD_AK'
+
+    Parameters
+    ----------
+    layer_val : 1-D array
+        Species columnar cencentrations at every layer.
+        Unit is like molec/cm^2, DU, ...
+    PEdge_Bot : 1-D array or None
+        Bottom edge pressure of every layer.
+        Layer index starts from 0.
+        Pressure decreases as layer index increases.
+    SW_AK: 1-D array
+        Scattering weight or averaging kernel
+    SW_AK_press : 1-D array
+        Pressure of *SW_AK*
+    ind_l : float or None
+        Layer index of tropospause.  Layer index starts from 0.
+        If *ind_l* is None, *P_tropopause* should be given.
+    P_tropopause : float or None
+        Tropopause pressure. 
+        If *P_tropopause* is None, *ind_l* should be given.
+    var : str (default is 'AMF')
+        'AMF': *SW_AK* is scattering weight, and air mass factor
+            is calculated.
+        'VCD_AK': *SW_AK* is averaging kernel, and vertical column 
+            density that can be compared to the satellite retrieval.
+
+    Returns
+    -------
+    out_dict : dict
+       'AMF': Air mass factor if *var* is 'AMF'.
+       'VCD_AK': Vertical column density that can be compared to the
+           satellite retrieval.
+
+    """
+
+    out_dict = {}
+
+    # get data of species in the troposphere 
+    trop_data = tropospheric_layer_column_one(layer_val, ind_l=ind_l,
+            PEdge_Bot=PEdge_Bot, P_tropopause=P_tropopause, pressure=True)
+
+    # Interpolate scattering weight or averaging kernel
+    # to pressure of each tropospheric levels.
+    trop_press = trop_data['trop_PCenter']
+    trop_SW_AK = SW_AK_intep_one(SW_AK, SW_AK_press, trop_press)
+
+    # Calculate air mass factor or vertical column density 
+    # that can be compared to the satellite retrieval.
+    trop_layer_val = trop_data['trop_layer']
+    if (var == 'AMF'):
+
+        # Air mass factor
+        AMF_data = AMF_one(trop_layer_val, trop_SW_AK)
+        AMF = AMF_data['AMF']
+        out_dict['AMF'] = AMF
+
+    elif (var == 'VCD_AK'):
+
+        # Vertical column density that can be compared to
+        # the satellite retrieval.
+        VCD_AK = VCD_AK_one(trop_layer_val, trpo_SW_AK)
+        out_dict['VCD_AK'] = VCD_AK
+
+    else:
+
+        print(' - AMF_trop_one: *var* is {}'.format(var))
+        print('  *var*\'s vaule can be: ', ['AMF', 'VCD_AK'])
+        print('  If *var* is \'AMF\', *SW_AK* is scatter weight. ' +
+                'And air mass factor is calculated')
+        print('  If *var* is \'VCD_AK\', *SW_AK* is averaging kernel. ' +
+                'And vertical column density that can be compared to ' +
+                'the satellite retrieval is calculated.')
+
+    return out_dict
+#
+#------------------------------------------------------------------------------
+#
+def AMF_trop(layer_val_arr, PEdge_Bot_arr, SW_AK_arr, 
+        SW_AK_press, ind_l_arr=None,
+        P_tropopause_arr=None, var='AMF',
+        flag=None):
+    """
+
+    flag : 
+    """
+
+    # Field dimension
+    dim = layer_val_arr.shape
+    dim = dim[0:-1]
+
+    # total number of grid
+    N_grid = 1
+    for i in range(len(dim)):
+        N_grid *= dim[i]
+
+    # flag
+    if flag is None:
+        flag = np.full(dim, True)
+
+    # output dict
+    out_dict = {}
+    if (var in ['AMF', 'VCD_AK']):
+        out_dict[var] = np.full_like(flag, np.nan)
+    else:
+        print(' - AMF_trop: *var* is {}'.format(var))
+        print('  *var*\'s vaule can be: ', ['AMF', 'VCD_AK'])
+        print('  If *var* is \'AMF\', *SW_AK_arr* is scatter weight. ' +
+                'And air mass factor is calculated')
+        print('  If *var* is \'VCD_AK\', *SW_AK_arr* is averaging kernel. ' +
+                'And vertical column density that can be compared to ' +
+                'the satellite retrieval is calculated.')
+
+
+    # iterate every grid
+    for i in range(N_grid):
+
+        # get index
+        if (len(dim) == 1):
+            ind = i
+        elif (len(dim) == 2):
+            ind = (i // dim[1], i % dim[1])
+        else:
+            print(' - AMF_trop: dimension error.')
+            exit()
+
+        print(i, ind)
+
+        # only process data with flag is True
+        if (not flag[ind]):
+            pass
+
+        # prepare parameters for AMF_trop_one function.
+        layer_val = layer_val_arr[ind,:]
+        PEdge_Bot = PEdge_Bot_arr[ind,:]
+        SW_AK     = SW_AK_arr[ind, :]
+        if ind_l_arr is None:
+            ind_l = None
+        else: 
+            ind_l = ind_l_arr[ind]
+        if P_tropopause_arr is None:
+            P_tropopause = None
+        else:
+            P_tropopause = P_tropopause_arr[ind]
+
+        # calculate AMF or VCD_AK
+        data_one = AMF_trop_one(layer_val, PEdge_Bot, SW_AK, SW_AK_press, 
+                ind_l=ind_l, P_tropopause=P_tropopause, var=var)
+        out_dict[var][ind] = data_one[var]
+
+    return out_dict
+#
+#------------------------------------------------------------------------------
+#
--- model/trop.py
+++ model/trop.py
@@ -3,9 +3,11 @@ Created on Janunary 15, 2020
 
 @author: Yi Wang
 """
-
+#
+#------------------------------------------------------------------------------
+#
 def tropospheric_layer_column_one(layer_val, ind_l=None, 
-        PEdge_Bot=None, P_tropopause=None):
+        PEdge_Bot=None, P_tropopause=None, pressure=True):
     """ Calculate columnar concentations of each layer in
     the troposphere and total tropospheric column. If both
     *PEdge_Bot* and *P_tropopause* are given, center and 
@@ -26,25 +28,36 @@ def tropospheric_layer_column_one(layer_val, ind_l=None,
         Layer index starts from 0.
         Pressure decreases as layer index increases.
     P_tropopause : float or None
-        Tropopause pressure.
+        Tropopause pressure. 
+        If *P_tropopause* is None, *PEdge_Bot* and *ind_l* should
+        be given
+    pressure : bool (default is True)
+        Whether or not return edge and center pressures of each
+        layer in the troposphere
 
     Returns
     -------
     out_dict : dict
         'trop_layer'   : tropospherc column
         'trop_col'     : columnar concentations of each layer
-        If both *PEdge_Bot* and *P_tropopause* should be given.
-        'trop_PEdge'   : Edge pressures of each layer in the the troposphere.
-        'trop_PCenter' : Eenter pressures of each layer in the the troposphere.
+        If *pressure* is True.
+        'trop_PEdge'   : Edge pressures of each layer in the troposphere.
+        'trop_PCenter' : Center pressures of each layer in the troposphere.
 
     """
 
     out_dict = {}
 
-    # If ind_ls is None. calcualte it.
+    # If *ind_l* is None. calcualte it.
     # *PEdge_Bot* and *P_tropopause* should be given.
     if ind_l is None:
-        ind_l = find_tropopause_index_one(PEdge_Bot, P_tropopause)
+        if ( (PEdge_Bot is not None) and (P_tropopause is not None) ):
+            ind_l = find_tropopause_index_one(PEdge_Bot, P_tropopause)
+        else:
+            print(' - tropospheric_layer_column_one: *ind_l* is {}. ' + 
+                    'both *PEdge_Bot* and *P_tropopause* ' + 
+                    'should be given.'.format(ind_l))
+            exit()
 
     # index ceil
     ind_ceil = np.ceil(ind_l)
@@ -63,7 +76,24 @@ def tropospheric_layer_column_one(layer_val, ind_l=None,
     out_dict['trop_col'] = trop_col
 
     # center and edge pressures of each layer in the the troposphere.
-    if ( (PEdge_Bot is not None) and (P_tropopause is not None) ):
+    if pressure:
+
+        # Tropopause
+        if P_tropopause is None:
+
+            if ( (PEdge_Bot is not None) and (ind_l is not None) ):
+                #P_tropopause = \
+                #        find_tropopause_pressure_one(PEdge_Bot, ind_l)
+                print(' - tropospheric_layer_column_one: ' +
+                'function find_tropopause_pressure_one ' + 
+                'is not implemented yet.')
+                exit()
+            else:
+                print(' - tropospheric_layer_column_one: ')
+                print('   *pressure* is {}'.format(pressure))
+                print('   *P_tropopause* is {}'.format(P_tropopause))
+                print('   Both *PEdge_Bot* and *ind_l* should be given.')
+                exit()
     
         trop_PEdge   = np.append( PEdge_Bot[0:ind_ceil+1], P_tropopause )
         trop_PCenter = (trop_PEdge[0:-1] + trop_PEdge[1:]) * 0.5
@@ -71,7 +101,9 @@ def tropospheric_layer_column_one(layer_val, ind_l=None,
         out_dict['trop_PCenter'] = trop_PCenter
 
     return out_dict
-
+#
+#------------------------------------------------------------------------------
+#
 def find_tropopause_index_one(PEdge_Bot, P_tropopause):
     """ Find tropopause index for one pixel
 
@@ -107,3 +139,6 @@ def find_tropopause_index_one(PEdge_Bot, P_tropopause):
             / (PEdge_Bot[i-1] - PEdge_Bot[i]) )
 
     return ind
+#
+#------------------------------------------------------------------------------
+#
