c01985e9f220321b2b979d520899557375317113
Yi Wang <ywang466@argon-login-2.hpc>
2020-03-03 11:54:22 -0600

    miscelleous

--- layout.py
+++ layout.py
@@ -4,6 +4,9 @@ Created on Feburary 4, 2020
 @author: Yi Wang
 """
 
+import cartopy.crs as ccrs
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+
 #
 #------------------------------------------------------------------------------
 #
@@ -96,6 +99,109 @@ def h_2_ax(fig, ax1, ax2, y_off=-0.1, ratio=0.06, height=None):
 #
 #------------------------------------------------------------------------------
 #
+def panel_tick_label(ax_list, ncol, xticks=[], yticks=[],
+        xlabel='', ylabel='',
+        zero_direction_label=False,
+        dateline_direction_label=False, number_format='g',
+        degree_symbol=u'\u00B0',
+        ):
+    """ Add ticks and labels to the right and bottom sides
+    of panel plots.
+    (Yi Wang, 02/27/2020)
+
+    Parameters
+    ----------
+    ax_list : list
+        All axes are stored in the list.
+    ncol : int
+        column number of the figure
+    xticks : list-like
+        xticks
+    yticks : list-like
+        yticks
+    xlabel : str
+        xlabel
+    ylabel : str
+        ylabel
+    zero_direction_label :
+        Direction label at 0 degree longitude
+    dateline_direction_label :
+        Direction label at 180 degree longitude
+    number_format :
+    degree_symbol :
+
+    Returns
+    ------
+
+    """
+
+    # xflag
+    xflag = [False] * len(ax_list)
+    for i in range(ncol):
+        xflag[-1-i] = True
+
+    # yflag
+    yflag = []
+    for i in range(len(ax_list)):
+        if (i % ncol) == 0:
+            yflag.append(True)
+        else:
+            yflag.append(False)
+
+    if ax_list is None:
+        return xflag, yflag
+
+
+    # Tick labels
+    tick_proj = ['PlateCarree', 'Mercator']
+
+    # Tick format for projection
+    lon_formatter = LongitudeFormatter(
+            zero_direction_label=zero_direction_label,
+            dateline_direction_label=dateline_direction_label,
+            number_format=number_format, degree_symbol=degree_symbol)
+    lat_formatter = LatitudeFormatter(number_format=number_format,
+            degree_symbol=degree_symbol)
+
+    # add ticks and labels
+    for i in range(len(ax_list)):
+
+        ax = ax_list[i]
+
+        # projection
+        try:
+            proj = str(type(ax.projection)).split('.')[-1][0:-2]
+        except:
+            proj = None
+
+        if xflag[i]:
+
+            # xticks
+            if proj in tick_proj:
+                ax.set_xticks(xticks, crs=ccrs.PlateCarree())
+                ax.xaxis.set_major_formatter(lon_formatter)
+            else:
+                ax.set_xticks(xticks)
+
+            # xlabel
+            ax.set_xlabel(xlabel)
+
+        if yflag[i]:
+
+            # yticks
+            if proj in tick_proj:
+                ax.set_yticks(yticks, crs=ccrs.PlateCarree())
+                ax.yaxis.set_major_formatter(lat_formatter)
+            else:
+                ax.set_yticks(yticks)
+
+            # ylabel
+            ax.set_ylabel(ylabel)
+
+    return xflag, yflag
+#
+#------------------------------------------------------------------------------
+#
 def right_center_label(ax, label):
     """ Add *label* to the right center of *ax*.
     This function is usually used to add unit label
@@ -105,13 +211,80 @@ def right_center_label(ax, label):
     ----------
     ax : axes
     label : str
-
     
     """
 
     ax.yaxis.set_label_position('right')
     ax.set_ylabel(label, rotation=0, ha='left', va='center')
 
+#
+#------------------------------------------------------------------------------
+#
+def multiFigure(nRow, nCol, **kwargs):
+    """
+    This function creates multi-figure layout...
+	
+    Parameter:
+    nRow	: row number of the figure
+    nCol	: column number of the figure
+	
+    Optional Paras:
+    proj	: projection method (current only allow one projection method
+    nUnit	: number of grid per figure
+    figsize : figure size
+    nGap	: number of grid between each figure
+	
+    Output:
+	
+    figure  : figure instance 
+    axes	: list of axes instance
+    proj	: projection method (current only allow one projection method)
+	
+    """
+    import cartopy.crs as ccrs
+    import matplotlib.pyplot as plt
+    import matplotlib.gridspec as gridspec
+	
+    proj = kwargs.get('proj', ccrs.PlateCarree())
+    nUint = kwargs.get('nUint', 1)
+    nGap = kwargs.get('nGap', 0)	
+    figsize = kwargs.get('figsize', (8, 6))	
+    projPos = kwargs.get('projPos', [])
+	
+    axes = []
+    fig = plt.figure(figsize = figsize)
+
+    nRow_grid = nRow * nUint
+    nCol_grid = nCol * nUint
+    left   = kwargs.get('left', None)
+    right  = kwargs.get('right', None)
+    top    = kwargs.get('top', None)
+    bottom = kwargs.get('bottom', None)
+    wspace = kwargs.get('wspace', None)
+    hspace = kwargs.get('hspace', None)
+    gs = fig.add_gridspec(nRow_grid, nCol_grid, left=left, right=right,
+            top=top, bottom=bottom, wspace=wspace, hspace=hspace)
+
+	
+    for i in range(nRow):
+        for j in range(nCol):
+            numFigure = i * nCol + j
+            if numFigure in projPos:
+                instant = fig.add_subplot(gs[i*nUint:(i + 1)*nUint - nGap,  \
+                        j*nUint:(j + 1)*nUint - nGap], \
+                        projection=proj)
+            else:
+                instant = fig.add_subplot(gs[i*nUint:(i + 1)*nUint - nGap,  \
+                        j*nUint:(j + 1)*nUint - nGap]  )
+                
+            axes.append(instant)
+
+    out_dict = {}
+    out_dict['fig']  = fig
+    out_dict['axes'] = axes
+    out_dict['proj'] = proj
+
+    return out_dict
 #
 #------------------------------------------------------------------------------
 #
--- pro_omi_no2_l3/plot_omi_no2_l3.py
+++ pro_omi_no2_l3/plot_omi_no2_l3.py
@@ -8,8 +8,9 @@ import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 import numpy as np
 
-from mylib.cartopy_plot import add_geoaxes, pcolormesh
+from mylib.cartopy_plot import add_geoaxes, pcolormesh, cartopy_plot
 from mylib.grid_utility import generate_grid, get_center_index
+from mylib.layout import multiFigure, h_2_ax, panel_tick_label
 import mylib.pro_omi_no2_l3.io_omi_no2_l3 as io
 
 def plot_NO2_monthly_mean(ax, lat_e, lon_e, NO2):
@@ -116,9 +117,9 @@ def plot_NO2_combine(dir_mean, dir_month,
             + r', NO$_2$'
     ax1 = add_geoaxes(fig, 311, xtick=[], ytick=ytick, title=title1)
     ax_list.append(ax1)
-    mesh1 = pcolormesh(ax1, lon_e, lat_e, NO2_mean,
+    pout1 = pcolormesh(ax1, lon_e, lat_e, NO2_mean,
             vmin=vmin_mean, vmax=vmax_mean)
-    cbar1 = fig.colorbar(mesh1, ax=ax1, extend='max',
+    cbar1 = fig.colorbar(pout1['mesh'], ax=ax1, extend='max',
             pad=pad, aspect=aspect)
     cbar1.set_label(r'[DU]')
 
@@ -126,10 +127,10 @@ def plot_NO2_combine(dir_mean, dir_month,
     title2 = year + ', ' + month + ', anomaly'
     ax2 = add_geoaxes(fig, 312, xtick=[], ytick=ytick, title=title2)
     ax_list.append(ax2)
-    mesh2 = pcolormesh(ax2, lon_e, lat_e, NO2_ano, 
+    pout2 = pcolormesh(ax2, lon_e, lat_e, NO2_ano, 
             cmap=plt.get_cmap('seismic'),
             vmin=vmin_ano, vmax=vmax_ano)
-    cbar2 = fig.colorbar(mesh2, ax=ax2, extend='both',
+    cbar2 = fig.colorbar(pout2['mesh'], ax=ax2, extend='both',
             pad=pad, aspect=aspect)
     cbar2.set_label(r'[DU]')
 
@@ -137,10 +138,10 @@ def plot_NO2_combine(dir_mean, dir_month,
     title3 = year + ', ' + month + ', relative anomaly'
     ax3 = add_geoaxes(fig, 313, xtick=xtick, ytick=ytick, title=title3)
     ax_list.append(ax3)
-    mesh3 = pcolormesh(ax3, lon_e, lat_e, NO2_ano_percent, 
+    pout3 = pcolormesh(ax3, lon_e, lat_e, NO2_ano_percent, 
             cmap=plt.get_cmap('seismic'),
             vmin=vmin_ano_per, vmax=vmax_ano_per)
-    cbar3 = fig.colorbar(mesh3, ax=ax3, extend='both', 
+    cbar3 = fig.colorbar(pout3['mesh'], ax=ax3, extend='both', 
             pad=pad, aspect=aspect)
     cbar3.set_label(r'[%]')
 
@@ -169,6 +170,142 @@ def plot_NO2_combine(dir_mean, dir_month,
 
 
     return fig
+
+def plot_monthly_anomaly_for_6yrs(dir_mean, dir_month,
+        start_year, end_year, year_list, month,
+        verbose=False, region_limit=[-90.0, -180.0, 90.0, 180.0], 
+        left=0.05, right=0.98, top=0.9, bottom=0.1,
+        wspace=0.05, hspace=0.05,
+        y_off=-0.06,
+        dir_fig=None, vmin_ano=-0.1, vmax_ano=0.1,
+        xticks=np.arange(-180.0, 180.1, 20.0),
+        yticks=np.arange(-90.0, 90.1, 10.0),
+        name='',
+        vmin_ano_per=-50.0, vmax_ano_per=50.0):
+    """ Plot monhtly anomaly for 6 years
+
+    """
+
+    # used to convert unit
+    toDU = 2.69e16
+
+    # read monthly data
+    NO2_month_dict = {}
+    for year in year_list:
+        file_month = dir_month + 'OMI_NO2_' + year + '-' +  month \
+                + '_monthly.nc'
+        NO2_month = io.read_month_OMI_NO2_L3(file_month, verbose=verbose)
+        NO2_month = NO2_month / toDU
+        NO2_month_dict[year] = NO2_month
+
+    # read mutli-year mean of monthly NO2
+    NO2_mean = io.read_multi_year_month_OMI_NO2_L3(dir_mean,
+            start_year, end_year, month, verbose=verbose)
+    NO2_mean = NO2_mean / toDU
+
+    # NO2 anomaly and relative anoamly
+    NO2_ano_dict = {}
+    NO2_ano_percent_dict = {}
+    for year in year_list:
+        # anomaly
+        NO2_ano = NO2_month_dict[year] - NO2_mean
+        NO2_ano_dict[year] = NO2_ano
+        # relative anomaly
+        NO2_ano_percent = NO2_ano / NO2_mean * 100.0
+        NO2_ano_percent_dict[year] = NO2_ano_percent
+
+    # generate grid
+    nlat_c, nlon_c = 720, 1440
+    lat_e, lon_e, lat_c, lon_c = generate_grid(nlat_c, nlon_c)
+
+    # get region data
+    i1 = get_center_index(lat_e, region_limit[0])
+    i2 = get_center_index(lat_e, region_limit[2])
+    j1 = get_center_index(lon_e, region_limit[1])
+    j2 = get_center_index(lon_e, region_limit[3])
+    lat_e = lat_e[i1:i2+2]
+    lon_e = lon_e[j1:j2+2]
+
+
+    # begin plot
+    nrow = 3
+    ncol = 4
+    figsize = (12,6)
+    projPos = list(range(nrow * ncol))
+    layout_dict = multiFigure(nrow, ncol,
+            left=left, right=right, top=top, bottom=bottom,
+            wspace=wspace, hspace=hspace,
+            figsize=figsize, projPos=projPos)
+    fig  = layout_dict['fig']
+    axes = layout_dict['axes']
+
+    ano_ax_ind     = [0, 1, 4, 5,  8,  9]
+    ano_per_ax_ind = [2, 3, 6, 7, 10, 11]
+    pout1_list = []
+    pout2_list = []
+    for iyr in range(len(year_list)):
+
+        year = year_list[iyr]
+
+        # anomaly
+        NO2_ano = NO2_ano_dict[year][i1:i2+1,j1:j2+1]
+        pout1 = cartopy_plot(lon_e, lat_e, NO2_ano, ax=axes[ano_ax_ind[iyr]], \
+                vmin=vmin_ano, vmax=vmax_ano, cbar=False, \
+                title=year+month, cmap=plt.get_cmap('seismic'))
+        pout1_list.append(pout1)
+
+        # relative anomaly
+        NO2_ano_percent = NO2_ano_percent_dict[year][i1:i2+1,j1:j2+1]
+        pout2 = cartopy_plot(lon_e, lat_e, NO2_ano_percent, \
+                ax=axes[ano_per_ax_ind[iyr]], cbar=False, \
+                vmin=vmin_ano_per, vmax=vmax_ano_per, \
+                title=year+month, cmap=plt.get_cmap('seismic'))
+        pout2_list.append(pout2)
+
+
+    states_provinces = cfeature.NaturalEarthFeature(
+            category='cultural',
+            name='admin_1_states_provinces_lines',
+            scale='50m',
+            facecolor='none')
+    # ticks
+    panel_tick_label(axes, ncol, xticks=xticks, yticks=yticks)
+
+    # set limit
+    for ax in axes:
+        ax.set_xlim([lon_e[0], lon_e[-1]])
+        ax.set_ylim([lat_e[0], lat_e[-1]])
+        ax.add_feature(cfeature.BORDERS)
+        ax.add_feature(states_provinces, edgecolor='k', linewidth=0.5)
+        ax.add_feature(cfeature.COASTLINE, zorder=200)
+        ax.add_feature(cfeature.OCEAN, color='w', zorder=100)
+
+    # anomaly colorbar
+    cax1 = h_2_ax(fig, pout1_list[4]['ax'], pout1_list[5]['ax'],
+            y_off=y_off)
+    cb1 = plt.colorbar(pout1_list[4]['mesh'], cax=cax1, \
+            orientation='horizontal', extend='both')
+    cb1.set_label('OMI NO2 anomaly [DU]')
+
+    # relative anomaly colorbar
+    cax2 = h_2_ax(fig, pout2_list[4]['ax'], pout2_list[5]['ax'],
+            y_off=y_off)
+    cb2 = plt.colorbar(pout2_list[4]['mesh'], cax=cax2, \
+            orientation='horizontal', extend='both')
+    cb2.set_label('OMI NO2 relative anomaly [%]')
+
+    # save figure
+    if dir_fig is not None:
+        figname = dir_fig + name + 'NO2_anomaly_6yrs_' + month \
+                + '_from_' + start_year + '-' + end_year + '.png'
+    plt.savefig(figname, format='png', dpi=300)
+
+
+
+
+
+
+
 
 
 
