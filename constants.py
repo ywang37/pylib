b487c2b40dec03b3640682c52166ce3ec3cb8f86
Yi Wang <ywang466@argon-login-1.hpc>
2020-05-13 17:47:28 -0500

    add kg_NO_to_ng_N

--- constants.py
+++ constants.py
@@ -13,6 +13,10 @@ avo = 6.02214076e23
 
 # molecular weight
 mol_N = 14.0 # Nitrigon
+mol_O = 16.0 # Oxygen
+
+
+mol_NO = 30.0 # Nitric oxide
 
 ##################
 # unit conversions
@@ -26,3 +30,6 @@ molec_cm2_to_DU = 1.0 / 2.69e16
 
 # molec (NOx) => kg (N)
 molec_to_kgN = 1.0 / avo * mol_N / 1e3
+
+# kg NO => ng N
+kg_NO_to_ng_N = 1e12 / mol_NO * mol_N
